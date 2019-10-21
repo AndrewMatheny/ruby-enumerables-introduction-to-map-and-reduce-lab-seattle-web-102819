c7e84359e032863c3ac6db3f9db9d7160466eb25
Andrew Matheny <androidmatheny@gmail.com>
2019-10-21 01:13:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,3 +27,13 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_square(source_array)
+  new_array = Array.new
+  counter = 0 
+  while counter < source_array.length do
+    new_array << source_array[counter] ** 2
+    counter += 1 
+  end
+  new_array
+end
